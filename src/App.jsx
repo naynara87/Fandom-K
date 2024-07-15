77a70313aae4c2221e21e8c3da141a56d0c25822
nayoonju <bubblenana@bubblecon.io>
2024-07-15 10:47:36 +0900

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import Landing from './pages/Landing/Landing';
-import List from './pages/List/List';
-=======
 import Landing from './pages/Landing';
 import List from './pages/List';
->>>>>>> 596ea655853c4c5e975862786861ea9c2bf04e24
 import Mypage from './pages/Mypage/Mypage';
 
 function App() {
