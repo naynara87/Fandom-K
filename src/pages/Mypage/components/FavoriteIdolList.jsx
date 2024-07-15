034bb5a16f39c8c71c91d3fac07f44c285692805
Jeongmin Oh <ojm51@naver.com>
2024-07-15 18:02:20 +0900

    fix: 로컬 스토리지에데이터가 저장되지 않는 문제 해결

--- src/pages/Mypage/components/FavoriteIdolList.jsx
+++ src/pages/Mypage/components/FavoriteIdolList.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
+
 import './FavoriteIdolList.css';
+
 import MyFavoriteIdolList from './MyFavoriteIdolList';
 import AddFavoriteIdolList from './AddFavoriteIdolList';
 
 function FavoriteIdolList() {
-  const [myFavoriteIdolList, setMyFavoriteIdolList] = useState([]);
+  const localStorageData = JSON.parse(localStorage.getItem('my-favorite-idol')) || [];
+  const [myFavoriteIdolList, setMyFavoriteIdolList] = useState(localStorageData);
 
   // 초기 로컬 스토리지에서 '내가 관심 있는 아이돌' 데이터 불러오기
-  useEffect(() => {
-    const localStorageData = JSON.parse(localStorage.getItem('my-favorite-idol')) || [];
-    setMyFavoriteIdolList(localStorageData);
-  }, []);
+  // useEffect(() => {
+  //   setMyFavoriteIdolList(localStorageData);
+  // }, []);
 
   // myFavoriteIdolList 값이 변할 때마다 로컬 스토리지에 저장
   useEffect(() => {
@@ -20,11 +22,11 @@ function FavoriteIdolList() {
   return (
     <div>
       <section className="section-my-favorite-idol">
-        <h1 className="list-title">내가 관심 있는 아이돌</h1>
+        <h3 className="list-title">내가 관심 있는 아이돌</h3>
         <MyFavoriteIdolList myFavoriteIdolList={myFavoriteIdolList} setMyFavoriteIdolList={setMyFavoriteIdolList} />
       </section>
       <section>
-        <h1 className="list-title">관심 있는 아이돌을 추가해보세요.</h1>
+        <h3 className="list-title">관심 있는 아이돌을 추가해보세요.</h3>
         <AddFavoriteIdolList myFavoriteIdolList={myFavoriteIdolList} setMyFavoriteIdolList={setMyFavoriteIdolList} />
       </section>
     </div>
