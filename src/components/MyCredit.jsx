e34b44f1da7797bf9bff0c858105535be8bb5589
Minjo Kwawk <iclock1960@naver.com>
2024-07-17 12:52:35 +0900

    feat : 내 크레딧 컴포넌트 기능구현 및 CSS 스타일링 완료

--- src/components/MyCredit.jsx
+++ src/components/MyCredit.jsx
@@ -1,26 +1,42 @@
 import './MyCredit.css';
 import CreditIcon from '../assets/images/donations_credit.svg';
+import { useState, useEffect } from 'react';
+import CreditModal from './CreditModal';
+
 function MyCredit() {
+  const initialCredit = () => {
+    const storedCredit = localStorage.getItem('myCredit');
+    return storedCredit ? JSON.parse(storedCredit) : 0;
+  };
+
+  const [credit, setCredit] = useState(initialCredit);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem('myCredit', JSON.stringify(credit));
+  }, [credit]);
+
+  const handleRecharge = (amount) => {
+    setCredit(credit + amount);
+    setIsModalOpen(false); // 충전 후 모달을 닫습니다.
+  };
 
   return (
-  <div className="credit-container">
-    <div className="credit-wrapper">
-      <div className="credit-balance-wrapper">
-        <p className="credit-title">내 크레딧</p>
-        <div className="credit-balance">
-          <img src={CreditIcon} alt="크레딧 아이콘" />
-          <p>36,000</p>
+    <div className="credit-container">
+      <div className="credit-wrapper">
+        <div className="credit-balance-wrapper">
+          <p className="credit-title">내 크레딧</p>
+          <div className="credit-balance">
+            <img src={CreditIcon} alt="크레딧 아이콘" />
+            <p>{credit.toLocaleString()}</p>
           </div>
+        </div>
+        <div className="credit-recharge-btn-container">
+          <button className="credit-recharge-btn" onClick={() => setIsModalOpen(true)}>충전하기</button>
+        </div>
       </div>
-    
-    
-    <div className="credit-recharge-btn-conainer">
-      <button>
-      충전하기
-      </button>
+      {isModalOpen && <CreditModal onClose={() => setIsModalOpen(false)} onRecharge={handleRecharge} />}
     </div>
-    </div>
-  </div>
   );
 }
 
