45d229f9108402896035e03f6abb7ca6a3122606
nayoonju <bubblenana@bubblecon.io>
2024-07-19 03:18:03 +0900

    feat: 투표하기 버튼을 누르면 해당 투표 내용 실시간 반영

--- src/pages/List/components/ChartVoteModal/ChartVoteModal.jsx
+++ src/pages/List/components/ChartVoteModal/ChartVoteModal.jsx
@@ -3,7 +3,7 @@ import "./ChartVoteModal.css";
 import IdolDetail from "../IdolDetail";
 import handleVote from "../../../../service/voteApi";
 
-function ChartVoteModal({ closeModal, idolRank, gender }) {
+function ChartVoteModal({ closeModal, idolRank, gender, updateIdolRank }) {
   const [selectedIdolId, setSelectedIdolId] = useState(null);
 
   useEffect(() => {
@@ -21,6 +21,7 @@ function ChartVoteModal({ closeModal, idolRank, gender }) {
       try {
         await handleVote(selectedIdolId); // Call handleVote with selected idolId
         console.log(`Successfully voted for idol with id: ${selectedIdolId}`);
+        updateIdolRank(); // 차트 데이터 실시간 업데이트
       } catch (error) {
         console.error("Failed to vote:", error);
         // Handle error (e.g., show an error message to the user)
@@ -34,7 +35,7 @@ function ChartVoteModal({ closeModal, idolRank, gender }) {
     <div className="modal-overlay">
       <div className="chart-modal">
         <div className="modal-header">
-          <h2>이달의 {gender === 'female' ? '여자' : '남자'} 아이돌</h2>
+          <h2>이달의 {gender === "female" ? "여자" : "남자"} 아이돌</h2>
           <button
             className="close-btn"
             onClick={closeModal}
