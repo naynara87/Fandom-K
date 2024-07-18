ea5cf3a74d88a750c24f17ee37a310ee44571ba5
marie1016 <kjo5281@gmail.com>
2024-07-18 20:54:41 +0900

    feat: 후원하기 POST

--- src/components/CreditContextProvider.jsx
+++ src/components/CreditContextProvider.jsx
@@ -21,11 +21,33 @@ function CreditContextProvider({ children }) {
   const handleReceivedDonationsUpdate = (newReceivedDonations) => {
     setLocalReceivedDonations(newReceivedDonations);
     //객체의 속성 업데이트
-    setSelectedDonation((prevSelectedDonation) => ({
-      ...prevSelectedDonation,
+    const updatedSelectedDonation = {
+      ...selectedDonation,
       receivedDonations: newReceivedDonations,
-    }));
-    localStorage.setItem("selectedDonation", JSON.stringify(selectedDonation));
+    };
+    setSelectedDonation(updatedSelectedDonation);
+    sendPostRequest(updatedSelectedDonation);
+    console.log(JSON.stringify(updatedSelectedDonation));
+  };
+
+  const sendPostRequest = (updatedSelectedDonation) => {
+    fetch("https://fandom-k-api.vercel.app/8-3/donations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // 필요한 경우 인증 토큰 등의 헤더도 추가할 수 있습니다.
+      },
+      body: JSON.stringify(updatedSelectedDonation),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("서버 응답:", data);
+        // 성공적으로 업데이트되었음을 사용자에게 알릴 수 있습니다.
+      })
+      .catch((error) => {
+        console.error("POST 요청 에러:", error);
+        // 오류 처리
+      });
   };
 
   const handleRecharge = (amount) => {
